57f66e764946ab7a04c28c99a71fa0b42d76446b
charlesxyt <charles.yitong@gmail.com>
2020-02-20 16:22:25 +1100

    use custom theme and add navigation bar

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import {ThemeProvider} from '@material-ui/styles'
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
+import theme from './ui/theme'
 import Header from './ui/Header'
+
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-        Hello
-    </div>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={() => <div>home</div>}></Route>
+          <Route exact path="/services" component={() => <div>services</div>}></Route>
+          <Route exact path="/customsoftware" component={() => <div>customsoftware</div>}></Route>
+          <Route exact path="/mobileapp" component={() => <div>mobileapp</div>}></Route>
+          <Route exact path="/websites" component={() => <div>websites</div>}></Route>
+          <Route exact path="/revolution" component={() => <div>revolution</div>}></Route>
+          <Route exact path="/about" component={() => <div>about</div>}></Route>
+          <Route exact path="/contact" component={() => <div>contact</div>}></Route>
+          <Route exact path="/estimate" component={() => <div>estimate</div>}></Route>
+
+        </Switch>
+      </BrowserRouter>
+      
+    </ThemeProvider>
   );
 } 
 
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {makeStyles} from '@material-ui/styles'
 import {
     AppBar,
     Toolbar,
-    useScrollTrigger
+    useScrollTrigger,
+    Tabs,
+    Tab,
+    Button
 } from '@material-ui/core'
+import {Link} from 'react-router-dom'
+
+import logo from '../../assets/logo.svg'
 
 
 function ElevationScroll(props) {
@@ -21,14 +28,85 @@ function ElevationScroll(props) {
   }
 
 
+  const useStyles = makeStyles(theme => ({
+      toolbarMargin: {
+          ...theme.mixins.toolbar,
+          marginBottom:"3em"
+      },
+      logo:{
+          height:"8em"
+      },
+      tabContainer:{
+          marginLeft:'auto'
+      },
+      tab:{
+          ...theme.typography.tab,
+          minWidth:10,
+          marginLeft:"25px"
+
+      },
+      button:{
+          ...theme.typography.estimate,
+          borderRadius:"50px",
+          marginLeft:"50px",
+          marginRight:"25px",
+          height:"45px"
+      },
+      logContainer:{
+          padding:0
+      }
+  }))
+
 
 export default function Header(props){
+
+    const classes = useStyles()
+    const [value,setValue] = useState(0)
+    useEffect(()=>{
+        if(window.location.pathname === "/" && value!== 0){
+            setValue(0)
+        }else if(window.location.pathname === "/services" && value!== 1){
+            setValue(1)
+        }else if(window.location.pathname === "/revolution" && value!== 2){
+            setValue(2)
+        }else if(window.location.pathname === "/about" && value!== 3){
+            setValue(3)
+        }else if(window.location.pathname === "/contact" && value!== 4){
+            setValue(4)
+        }else if(window.location.pathname === "/estimate" && value!== 5){
+            setValue(5)
+        }
+    },[value])
+
+    const handleChange = (event,valuePassed) => {
+        setValue(valuePassed)
+    }
+
     return (
-        <ElevationScroll>
-            <AppBar position="fixed">
-                <Toolbar>Arc Development</Toolbar>
-            </AppBar>
-        </ElevationScroll>
+        <React.Fragment>
+            <ElevationScroll>
+                <AppBar position="fixed">
+                    <Toolbar disableGutters>
+                        <Button component={Link} to="/" className={classes.logContainer} onClick={() => setValue(0)}>
+                            <img alt="company logo" className={classes.logo} src={logo}/>
+                        </Button>
+                        
+                        <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
+                            <Tab className={classes.tab} component={Link} to="/" label="Home"></Tab>
+                            <Tab className={classes.tab} component={Link} to="/services" label="Services"></Tab>
+                            <Tab className={classes.tab} component={Link} to="/revolution" label="The Revolution"></Tab>
+                            <Tab className={classes.tab} component={Link} to="/about" label="About us"></Tab>
+                            <Tab className={classes.tab} component={Link} to="/contact" label="Contact us"></Tab>
+                        </Tabs>
+                        <Button variant="contained" color="secondary" className={classes.button}>
+                            Free Estimate
+                        </Button>
+                    </Toolbar>
+                </AppBar>
+            </ElevationScroll>
+            <div className={classes.toolbarMargin}/>
+        </React.Fragment>
+        
         
     )
 }
