4cc3b404fa38b9ede1b3b42b15ba716ecef53d19
charlesxyt <charles.yitong@gmail.com>
2020-02-24 13:29:50 +1100

    fix bugs in the info section which is overflowed in small size

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -262,10 +262,9 @@ export default function LandingPage(props){
                    
                     style={{textAlign:matchesXS ? "center":undefined}} 
                     direction={matchesSM ? "column":"row"}
-                    spacing={matchesXS ? 10 : 0}
                     >
                         <Grid sm item style={{marginLeft:matchesXS ? 0 : matchesSM ? "2em":"5em"}} >
-                            <Grid container direction="column">
+                            <Grid container style={{marginBottom:matchesXS ? "10em":0}}  direction="column">
                                 <Typography variant="h2" style={{color:"white"}}>About us</Typography>
                                 <Typography variant="subtitle2">Let's get personal</Typography>
                                 <Grid item>
